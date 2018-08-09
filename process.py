867194eccf2d52300c69d4abe4f89df71722d2e6
tqtrinh414 <tranquoctrinh.041298@gmail.com>
2018-08-09 23:19:59 +0700

    update code

--- process.py
+++ process.py
@@ -5,16 +5,23 @@ import nltk
 from nltk import word_tokenize
 import pandas as pd
 
-addv = {'vp':'vice president', 'hr': 'human resouce', 'r d': 'research'
-            'qa': 'qa', 'qc':'qc', 'mgr':'manager', 'ceo':'ceo',
-        }
 
-PATTERN_1 = ["sales", "accounts", "business"]
-PATTERN_2 = ["accounting", "finance"]
+
+PATTERN_FUNC = {
+    "sales": ["sales", "account", "accounts", "business development"], 
+    "finance": ["accounting", "finance"], 
+    "it": ["programmer", "engineer", "developer", "tester", "it ", "web"],
+    "marketing": ["marketing"]
+}
+PATTERN_LEVEL = {
+    "manager": ["manager", "assistant manager"],
+    "directors": ["director", "associate director"]
+}
 
 
 def remove_special_character(str):
     return(re.sub('[^A-Za-z]+', ' ',str))
+
 
 def preprocessing(path = "Sample_data.csv"):
     df = pd.read_csv(path)
@@ -23,28 +30,100 @@ def preprocessing(path = "Sample_data.csv"):
     df = df.drop(['Function', 'Level'], axis = 1)
     return(df)
 
-def process2word(title):
-    title = [spell(w) for w in word_tokenize(title)]
-    w1 = title[0]
-    w2 = title[1]
-    function = ""
-    level = ""
-    if (w1 in PATTERN_1):
-        function = "sales"
-        level = w2
-    elif (w1 in PATTERN_2):
-        function = "finance"
-        level = w2
-    else:
-        function = w1
-        level = w2
-    return (function, level)
 
-def get_unique(df):
+def get_unique(df, pattern_func, pattern_level):
     uni_title = set(list(df['Title']))
     dct = {}
     for title in uni_title:
-        if len(title.split(' ')) == 1:
-            dct[title] = (title, "")
-        if len(title.split(' ')) == 2:
-            dct[title] = process2word(title)
+        dct[title] = ["",""]
+    
+    for title in uni_title:
+        flag = 1
+        for x in pattern_func:
+            pf = pattern_func[x]
+            for yf in pf:
+                if title.find(yf) != -1:
+                    dct[title][0] = x
+                    flag = 0
+        for x in pattern_level:
+            pl = pattern_level[x]
+            for yl in pl:
+                if title.find(yl) != -1:
+                    dct[title][1] = x
+                    flag = 0
+        
+        if (len(title.split(" ")) == 2 and flag): # title co 2 tu va khong trong pattern
+            temp_title = title.split()
+            dct[title] = (temp_title[0],temp_title[1])
+            cout += 1
+    return dct
+
+
+def get_result(df,dct):
+    funcs = []
+    levels = []
+    for x in df['Title']:
+        func = dct[x][0]
+        lvl = dct[x][1]
+        if (func != ""):
+            funcs.append(func)
+        else:
+            funcs.append(x)
+        if (lvl != ""):
+            levels.append(lvl)
+        else:
+            levels.append(x)
+
+    df['Function'] = funcs
+    df['Level'] = levels
+    df.to_csv("test.csv")
+
+
+def input_pattern_func():
+    dctPatFunc = {}
+    print ("input pattern cho function: ")
+    s = ""
+    while s != "0":
+        print ("function (nhap 0 de nhap function khac) ")
+        s = input()
+        s2 = ""
+        lst = []
+        while s != "0" and s2 != "0":
+            print ("keyword (nhap 0 de dung):")
+            s2 = input()
+            if s2 != "0":
+                lst.append(s2)
+        if s!= "0":
+            dctPatFunc[s] = lst
+    return dctPatFunc
+
+
+def input_pattern_level():
+    dctPatLevel = {}
+    print ("input pattern cho level: ")
+    s = ""
+    while s != "0":
+        print ("level (nhap 0 de nhap level khac) ")
+        s = input()
+        s2 = ""
+        lst = []
+        while s != "0" and s2 != "0":
+            print ("keyword (nhap 0 de dung):")
+            s2 = input()
+            if s2 != "0":
+                lst.append(s2)
+        if s!= "0":
+            dctPatLevel[s] = lst
+    return dctPatLevel
+
+
+def main():
+    #print (input_pattern_func())
+    #print (input_pattern_level())
+
+    df = preprocessing()
+    dct = get_unique(df, PATTERN_FUNC, PATTERN_LEVEL)
+    get_result(df, dct)
+
+if __name__ == "__main__":
+    main()
